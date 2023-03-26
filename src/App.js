c8b13554468a46bcc1ea9e9bc1fd40279c4e87a2
Varun Arya <varunarya119@gmail.com>
2023-03-26 14:36:20 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Banner } from "./components/Banner";
 import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Certifications } from './components/Certifications';
-import { Papers } from "./components/Papers";
+// import { Papers } from "./components/Papers";
 import { Experience } from "./components/Experience"; 
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
